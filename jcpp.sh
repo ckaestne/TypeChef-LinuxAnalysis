fbeb40978b446a60700f49ff04b8221353427b81
Christian Kaestner <christian.kaestner@uni-marburg.de>
2012-01-03 14:53:23 +0100

    update scripts

--- jcpp.sh
+++ jcpp.sh
@@ -7,14 +7,11 @@ fi
 # What you should configure
 javaOpts='$javaOpts -Xmx2G -Xms128m -Xss10m'
 
-macro_stats_path=macroDebug.txt
-debugsource_path=debugsource.txt
-
 # For Java compiled stuff!
 basePath=.
 
 #mainClass="org.anarres.cpp.Main"
-mainClass="de.fosd.typechef.linux.LinuxPreprocessorFrontend"
+mainClass="de.fosd.typechef.Frontend"
 
 # Brute argument parsing
 # The right thing to do would be to be a gcc replacement, parse its flags and
@@ -45,24 +42,8 @@ fi
 echo "==Partially preprocessing $inp"
 echo $partialPreprocFlags
 
-bash -c "time java -ea $javaOpts -cp \
-$basePath/project/boot/scala-2.9.0/lib/scala-library.jar:\
-$basePath/PartialPreprocessor/lib/gnu.getopt.jar:\
-$basePath/PartialPreprocessor/lib/junit.jar:\
-$basePath/lib/junit-4.8.1.jar:\
-$basePath/FeatureExprLib/lib_managed/scala_2.9.0/compile/org.sat4j.core-2.3.1.jar:\
-$basePath/lib/scalacheck_2.8.1-1.8.jar:\
-$basePath/lib/scalatest-1.2.jar:\
-$basePath/FeatureExprLib/target/scala_2.9.0/classes:\
-$basePath/ConditionalLib/target/scala_2.9.0/classes:\
-$basePath/PartialPreprocessor/target/scala_2.9.0/classes:\
-$basePath/ParserFramework/target/scala_2.9.0/classes:\
-$basePath/CParser/target/scala_2.9.0/classes:\
-$basePath/CTypeChecker/target/scala_2.9.0/classes:\
-$basePath/LinuxAnalysis/target/scala_2.9.0/classes:\
-$basePath/PreprocessorFrontend/target/scala_2.9.0/classes:\
-$basePath/ParserFramework/lib_managed/scala_2.9.0/compile/kiama_2.9.0-1.1.0.jar\
-  $mainClass -t -i \
+bash -c "time ./typechefLinux.sh -ea $javaOpts \
+  $mainClass \
   $(for arg in $partialPreprocFlags "$@"; do echo -n "\"$arg\" "; done) \
   '$inp' -o '$outPartialPreproc' 2> '$outErr' |tee '$outDbg'" \
   2> "$outTime" || true
