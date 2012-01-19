513dad82fd515b144e7a88349aeae129ebdee167
Christian Kaestner <kaestner@informatik.uni-marburg.de>
2012-01-19 16:36:52 +0100

    new tool: filter of type errors according to dimacs feature model

---
+++ src/main/scala/de/fosd/typechef/linux/FilterTypeErrors.scala
@@ -0,0 +1,42 @@
+package de.fosd.typechef.linux
+
+import featuremodel.LinuxDimacsModel
+import java.io.File
+import de.fosd.typechef.featureexpr.FeatureExprParser
+
+/**
+ * this class parses a .dbg file and filters type errors with satisfiable conditions according to the
+ * dimacs model
+ */
+
+object FilterTypeErrors extends App {
+
+    if (args.size != 1 || !new File(args(0)).exists) {
+        println(".dbg file expected as parameter")
+        sys.exit()
+    }
+
+    val fmDimacs = new LinuxDimacsModel().createFeatureModel
+
+
+    val lines = scala.io.Source.fromFile(args(0)).getLines()
+    while (lines.hasNext) {
+        val line: String = lines.next
+
+        if (line.startsWith("  -")) {
+            val formula = line.substring(7, line.indexOf(']'))
+
+            val fexpr = new FeatureExprParser().parse(formula)
+
+            if (fexpr.isSatisfiable(fmDimacs)) {
+                println(fexpr)
+                println(lines.next)
+                println()
+            }
+        }
+
+
+    }
+
+
+}
