3909e6ea468416a9dde646e824c706f8cc907884
Christian Kaestner <kaestner@cs.cmu.edu>
2013-01-23 20:27:06 -0500

    added performance measurement

--- src/main/scala/de/fosd/typechef/linux/CollectTimes.scala
+++ src/main/scala/de/fosd/typechef/linux/CollectTimes.scala
@@ -9,12 +9,16 @@ object CollectTimes extends App {
   val files = scala.io.Source.fromFile("linux_files.lst").getLines.toList
 
   for (file <- files) {
+    try {
     val vastats=scala.io.Source.fromFile("l/"+file+".dbg").getLines.toList.last
     val istats=scala.io.Source.fromFile("l/"+file+".i.dbg").getLines.toList.last
     val errors =(XML.load(new FileReader("l/"+file+".c.xml")) \ "_").size
     val ierrors =(XML.load(new FileReader("l/"+file+".i.xml")) \ "_").size
     val out= file+";"+vastats+";"+istats+";"+errors+";"+ierrors
-    println(out)
+    println(out) 
+  } catch {
+    case e:Exception => println("error "+file+";"+e.getMessage)
+  }
   }
 
 
