7191f7e1ef6751adf02ad028196a1e77375a9713
Christian Kastner <ckaestne@feature.isri.cmu.edu>
2014-06-18 11:42:28 -0400

    generated new models with own kconfigreader tool

--- genFMs.sh
+++ genFMs.sh
@@ -3,16 +3,16 @@
 thisdir=`pwd`
 echo $thisdir
 linuxDir=l
+dumpconf=$thisdir/../undertaker/scripts/kconfig/dumpconf
 
 cd $linuxDir
-for arch in arm x86 alpha  avr32   cris  h8300  m68k   microblaze  mn10300  powerpc  score  sparc    xtensa  blackfin  frv   ia64   m32r     m68knommu  mips        parisc   s390 sh     um
+for arch in arm x86 alpha  avr32   cris  h8300  m68k   microblaze  mn10300  powerpc  score  sparc    xtensa  blackfin  frv   ia64   m32r     m68knommu  mips        parisc   s390 sh   
 do
   export LINUXVERSION=2.6.33.3
   export ARCH=$arch
-  ../linux-variability-analysis-tools/exconfig/bin/exconf-i386 arch/$arch/Kconfig > $thisdir/pcs/$arch.exconfig
-  echo `wc -l $thisdir/pcs/$arch.exconfig`
-  ../linux-variability-analysis-tools/fm-translation/run.sh gsd.linux.tools.TristateTranslationMain $thisdir/pcs/$arch.exconfig $thisdir/pcs/$arch.bool2
-  ../linux-variability-analysis-tools/fm-translation/run.sh gsd.linux.tools.CNFMain $thisdir/pcs/$arch.bool2 $thisdir/pcs/$arch.var2.dimacs
+  echo ../../kconfigreader/run.sh de.fosd.typechef.kconfig.KConfigReader --writeDimacs --writeNonBoolean --dumpconf $dumpconf arch/$arch/Kconfig $thisdir/pcs/$arch
+  ../../kconfigreader/run.sh de.fosd.typechef.kconfig.KConfigReader --writeDimacs --writeNonBoolean --dumpconf $dumpconf arch/$arch/Kconfig $thisdir/pcs/$arch
+  wc -l $thisdir/pcs/$arch.model
 done
 
 cd $thisdir
