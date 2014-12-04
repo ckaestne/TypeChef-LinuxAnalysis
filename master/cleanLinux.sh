25d2d96fde0b71f61168d17f1261006c7f99cbe2
Christian Kaestner <kaestner@cs.cmu.edu>
2014-12-04 10:47:37 -0500

    update path

--- master/cleanLinux.sh
+++ master/cleanLinux.sh
@@ -1,1 +1,1 @@
-cat pcs/x86.flist |while read i; do echo l/$i; rm linux/$i.dbg; rm linux/$i.c.xml; rm linux/$i.pi; done 
+cat pcs/x86.flist |while read i; do echo linux/$i; rm linux/$i.dbg; rm linux/$i.c.xml; rm linux/$i.pi; done 
